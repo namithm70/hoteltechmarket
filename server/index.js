cb7fd4279f1361617c015a53ecaf653caeff9f85
Namith <namith@Namiths-MacBook-Air.local>
2026-01-02 18:45:49 +0530

    Fix wildcard route for Express 5

--- server/index.js
+++ server/index.js
@@ -37,7 +37,7 @@ if (process.env.NODE_ENV === 'production') {
     const clientDistPath = path.join(__dirname, '../client/dist');
     app.use(express.static(clientDistPath));
 
-    app.get('*', (req, res) => {
+    app.get('/*', (req, res) => {
         res.sendFile(path.join(clientDistPath, 'index.html'));
     });
 } else {
